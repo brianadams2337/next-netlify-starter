9212dbabf11677c7f2b0018918cf4e5f43932661
Brian Adams <2798669+brianadams2337@users.noreply.github.com>
2022-07-17 14:57:09 -0400

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -26,3 +26,13 @@ export default function Home() {
     </div>
   )
 }
+
+export async function getStaticProps() {
+  const products = await getProductList();
+
+  return {
+    props: {
+      products,
+    },
+  };
+}
