29992f61b32653a5410c09a317444e17a015c419
Brian Adams <2798669+brianadams2337@users.noreply.github.com>
2022-07-16 23:01:32 -0400

    update

--- pages/index.js
+++ pages/index.js
@@ -2,7 +2,7 @@ import Head from "next/head";
 import ProductListing from "@components/ProductListing";
 import Header from "@components/Header";
 import Footer from "@components/Footer";
-import { getProductList } from "@api/functions/utils/getProductList";
+import { getProductList } from "@api/getProductList";
 
 export default function Home() {
   return (
